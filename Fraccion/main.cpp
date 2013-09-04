daae39899c05371a508083d043281af97805a506
w. chorolque <wchorolque@gmail.com>
2013-09-04 11:05:52 -0400

    Initial commit

---
+++ Fraccion/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+class Fraccion
+{
+    private:
+        int numerador;
+        int denominador;
+};
+
+int main()
+{
+    Fraccion f;
+    cout << "Hello World!" << endl;
+    return 0;
+}
+
